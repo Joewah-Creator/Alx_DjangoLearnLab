e8379b82018a96e1c260d1747b09624e41fbccd9
josphat wahome <wahomejosphatt27@gmail.com>
2025-11-09 20:26:24 +0300

    fix librarian for library query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,25 +1,15 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
+
 
 def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+    return Book.objects.filter(author__name=author_name)
+
 
 def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    return Book.objects.filter(library__name=library_name)
+
 
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
 
-if __name__ == "__main__":
-    print("Books by J.K. Rowling:")
-    for book in books_by_author("J.K. Rowling"):
-        print(" -", book.title)
-
-    print("\nBooks in Central Library:")
-    for book in books_in_library("Central Library"):
-        print(" -", book.title)
-
-    librarian = librarian_for_library("Central Library")
-    print("\nLibrarian of Central Library:", librarian.name)
