107ae9c7774bad53c4d2b3b354198da9916867cc
josphat wahome <wahomejosphatt27@gmail.com>
2025-11-13 13:46:30 +0300

    restart the project

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Book, Library, Author
 
-admin.site.register(Book)
-admin.site.register(Library)
-admin.site.register(Author)
-
+# Register your models here.
