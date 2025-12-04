cb0df227c04d750db352fca1578cee0364f1a7af
Josphat Wahome <Wahomejosphatt27@gmail.com>
2025-12-05 01:01:35 +0300

    Fix : taggit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,30 +3,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils.text import slugify
 from django.urls import reverse
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=60, unique=True, blank=True)
-
-    class Meta:
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+# import taggit manager
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    # use taggit's manager
+    tags = TaggableManager(blank=True, related_name='posts')
 
     class Meta:
         ordering = ['-published_date']
@@ -37,14 +25,11 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'pk': self.pk})
 
+
 class Profile(models.Model):
-    """
-    Minimal Profile model. Uses CharField for avatar (path/URL) to avoid
-    requiring Pillow at import time. If you want file uploads, replace the
-    avatar field with ImageField and install Pillow (pip install Pillow).
-    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
+    # keep avatar as simple CharField to avoid Pillow issues for now
     avatar = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
@@ -55,7 +40,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     else:
-        # ensure profile exists and save
         Profile.objects.get_or_create(user=instance)
 
 class Comment(models.Model):
